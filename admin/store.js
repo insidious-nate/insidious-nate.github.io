473b13cb56cf1705890174388e1506ad83ec9c1b
insidious-nate <carlosjonathan74@gmail.com>
2022-10-30 22:23:00 +0200

    shopping cart

--- admin/store.js
+++ admin/store.js
@@ -1,20 +1,23 @@
 var storedItem = localStorage.getItem("storedItem");
  
 var stock = new Array(3);
+var cartNumber = 0;
 
 function store() {
     var package1 = "";
     //var cartNumber = document.getElementById("cartNo").value;
-    var cartNumber = 0;
+    
 
     document.getElementById("cartNo").innerHTML = cartNumber+"";
 
-    cartNumber++;
-    document.getElementById("cartNo").innerHTML = cartNumber+"";
+    //cartNumber++;
+    //document.getElementById("cartNo").innerHTML = cartNumber+"";
 
 
 }
 function checkOut () {
+    cartNumber++;
+    document.getElementById("cartNo").innerHTML = cartNumber+"";
     var shoppingCrt=true;
     //window.open("payement.html");
 }
